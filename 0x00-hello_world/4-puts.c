768430472c23a7bbeaf086e0198b120235c975d9
maryam <maryamhemami2@gmail.com>
2023-03-19 17:24:17 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <studio.h> 
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle"):	    
-    return (0):
+#include<stdio.h>
+\**
+* main – Entry point
+* Return:  Always 0 (Success)
+*/
+int main(void)
+{
+         Puts(“\”Programming is like building a multilingual puzzle”);
+     return (0);
 }
+
