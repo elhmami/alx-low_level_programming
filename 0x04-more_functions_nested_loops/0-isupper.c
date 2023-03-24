7eed32bc7ad03ebdb693e264af6b9027acb59127
maryam <maryamhemami2@gmail.com>
2023-03-24 16:51:36 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * _isdigit - checks if a character is digit
- * @c: carrier variable
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
  * Return: 1 if true, 0 if false
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-
-	else
-		return (0);
+	}
+	return (0);
 }
