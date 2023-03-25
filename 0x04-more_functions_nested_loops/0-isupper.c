cbab06687736f0b9d33c2443d0a30da783664a8f
Meriem Hemami <maryamhemami2@gmail.com>
2023-03-25 02:14:07 +0100

    task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * _isupper - uppercase letters
- * @c: character to check
+ * @c: char to check
  *
  * Return: 0 or 1
  */
-int	_isupper(int c)
+
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
